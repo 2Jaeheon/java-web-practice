f20b49fd71dc32fe224ac3ad5074eae9ede08978
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-07-15 02:25:08 +0900

    ADD: Using servlet to send data
    I learned how to transfer data using a servlet and learned about the GET method and POST method.

---
+++ pro06/src/main/java/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,49 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet3
+ */
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        System.out.println("init() method");
+    }
+
+    /**
+     * @see Servlet#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+        System.out.println("destroy() method");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setCharacterEncoding("utf-8");
+        String user_id = request.getParameter("user_id");
+        String user_pw = request.getParameter("user_pw");
+
+        System.out.println("아이디: " + user_id);
+        System.out.println("패스워드: " + user_pw);
+    }
+
+}
