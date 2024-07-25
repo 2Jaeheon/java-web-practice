006c73993e62831ea7cf99216eab8c99d78a46b7
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-07-25 23:10:30 +0900

    ADD: Forward learning
    Learn how to forward. There are redirect, refresh, location, and dispatch

---
+++ pro08/src/sec03/ex01/FirstServlet.java
@@ -0,0 +1,22 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first5")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        RequestDispatcher dispatch = req.getRequestDispatcher("second5?name=lee");
+        dispatch.forward(req, resp);
+    }
+}
