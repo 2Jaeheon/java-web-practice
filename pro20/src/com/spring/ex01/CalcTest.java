8670d8ad8cfcf68fa0a262449c7c55a760bf3904
JaeheonLee <121741455+2Jaeheon@users.noreply.github.com>
2024-09-04 22:04:47 +0900

    FIX: description
    Added a description (comment) of the grammar

--- pro20/src/com/spring/ex01/CalcTest.java
+++ pro20/src/com/spring/ex01/CalcTest.java
@@ -8,6 +8,12 @@ public class CalcTest {
 
     public static void main(String[] args) {
         PropertyConfigurator.configure("log4j.properties");
+        //	ClassPathXmlApplicationContext는 클래스 경로에서 XML 설정 파일을 로드하는 데 사용됩니다.
+        //	이는 설정 파일이 애플리케이션의 클래스 경로(즉, src/main/resources와 같은 디렉토리)에 위치할 때 주로 사용됩니다.
+
+        //	FileSystemResource는 파일 시스템의 특정 경로에서 설정 파일을 로드할 때 사용됩니다.
+        //	XmlBeanFactory는 XML 파일을 통해 스프링 빈을 정의하고 관리하는 스프링 컨테이너입니다.
+        //	하지만 스프링 3.1 이후부터는 ApplicationContext가 더 권장되는 방식입니다.
         ApplicationContext context = new ClassPathXmlApplicationContext("AOPTest.xml");
         Calculator cal = (Calculator) context.getBean("proxyCal");
         cal.add(100, 20);
--- pro20/src/com/spring/ex01/LoggingAdvice.java
+++ pro20/src/com/spring/ex01/LoggingAdvice.java
@@ -5,10 +5,13 @@ import org.aopalliance.intercept.MethodInvocation;
 
 public class LoggingAdvice implements MethodInterceptor {
 
+    //invoke 메서드는 MethodInterceptor 인터페이스에서 정의된 메서드로, 실제로 메서드 호출을 가로채는 핵심 메서드
+    //MethodInvocation 객체를 인자로 받아, 원래 메서드 호출에 관한 정보와 실행 제어를 제공합니다.
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        System.out.println("[메서드 호출 전 : LoggingAdvice");
+        System.out.println("[메서드 호출 전 : Logging Advice");
         System.out.println(invocation.getMethod() + "메서드 호출 전");
 
+        //invocation.proceed()를 호출하여 실제 대상 메서드를 실행
         Object object = invocation.proceed();
 
         System.out.println("[메서드 호출 후 : loggingAdvice");
